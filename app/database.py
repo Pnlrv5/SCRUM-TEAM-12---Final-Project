153f6ac7a342d1ac9a0c725c44afefc56ba4f3e1
Will Amlong <amlongwi@gmail.com>
2025-12-11 22:26:39 -0600

    changed database.py to Flask and SQLAlchemy, added ticket_code.py which generates random Eticket, edited both new_reservation route and html to more closely match example videp. Finally new reservation system works and updates reservation.db

--- app/database.py
+++ app/database.py
@@ -1,12 +1,17 @@
-import mysql.connector
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-def get_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="password", 
-        database="flights"
-    )
+class Reservation(db.Model):
+    __tablename__ = "reservations"
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    seat_row = db.Column(db.Integer, nullable=False)
+    seat_col = db.Column(db.String(1), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    reservation_code = db.Column(db.String(20), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"<Reservation {self.first_name} {self.last_name} Seat {self.seat_row}{self.seat_col}>"
